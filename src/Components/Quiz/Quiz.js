d1a3cdf8290af57c413de06a9f1fb794d4b1d970
aliftareq <aliftareq@gmail.com>
2022-10-11 18:19:29 +0600

    icon set up

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { EyeIcon } from '@heroicons/react/24/solid'
 
 const Quiz = ({ value, quiz }) => {
     console.log(quiz);
     const { question, options, correctAnswer } = quiz;
     const [op1, op2, op3, op4] = options;
     return (
-        <div className="px-10 py-20 text-center border rounded lg:px-5 lg:py-10 xl:py-20">
-            <h3 className="max-w-xs mx-auto mb-2 font-bold text-gray-700">
-                Quiz {value} : {question}
-            </h3>
+        <div className="px-10 py-10 text-center border rounded lg:px-5 lg:py-10 xl:py-10">
+            <div className='flex justify-between'>
+                <h3 className="w-4/5 mx-auto mb-2 font-bold text-gray-700">
+                    Quiz {value} : {question}
+                </h3>
+                <EyeIcon className="h-8 w-1/5 text-green-500" />
+            </div>
+
             <form action="">
                 {
                     <label className="flex border-2 border-gray-500 p-2 rounded-md mt-2" htmlFor={op1}>
